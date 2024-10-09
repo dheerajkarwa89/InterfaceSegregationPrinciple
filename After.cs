352189ad3b3a66f697fb30d1bd61e5ca706a44df
Dheeraj <dheerajkarwa89@gmail.com>
2024-10-09 15:59:40 +0530

    Add project files.

---
+++ After.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+    public interface IPrinter
+    {
+        void Print();
+    }
+
+    public interface IScanner
+    {
+        void Scan();
+    }
+
+    public interface IFax
+    {
+        void Fax();
+    }
+
+    public class _OldPrinter : IPrinter
+    {
+        public void Print()
+        {
+            //print logic
+        }
+    }
+}
---
+++ Before.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+    public interface IMultiFunctionDevice
+    {
+        void Print();
+
+        void Scan();
+
+        void Fax();
+    }
+
+    public class OldPrinter : IMultiFunctionDevice
+    {        
+        public void Print()
+        {
+            //print logic
+        }
+
+        public void Scan()
+        {
+            //scan logic
+        }
+
+        public void Fax()
+        {
+            //fax logic
+        }
+    }
+}
